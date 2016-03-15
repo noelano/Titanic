92ec351ca159105bf10d9270b0e51c95b17e2f48
noelano <noelanorodriguez@gmail.com>
2016-03-15 11:44:04 +0000

    Data exploration intro

---
+++ PandasScripts/DataExploration.py
@@ -0,0 +1,24 @@
+""" Some initial exploratory analysis of the input data
+Author : Noel
+Date : 15th March 2016
+
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv(r'..\Data\train.csv')
+
+print (df.head(5))
+print(df.describe())
+
+# Summaries of distinct features
+print(df['Survived'].value_counts())
+print(df['Pclass'].value_counts())
+print(df['Embarked'].value_counts())
+
+# Compare age with survival and class with survival
+
+plt.scatter(df['Fare'], df['Age'], c=df['Survived'])
+plt.legend()
+plt.show()
